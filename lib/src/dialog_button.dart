370cce188fc2f27eebed2553cd89bfe8326dc527
guiolifer <guiooliger@gmail.com>
2023-05-11 16:17:49 -0300

    fixed color dart 3

--- lib/src/dialog_button.dart
+++ lib/src/dialog_button.dart
@@ -57,7 +57,7 @@ class DialogButton extends StatelessWidget {
       width: width,
       height: height,
       decoration: BoxDecoration(
-        color: color ?? Theme.of(context).accentColor,
+        color: color,
         gradient: gradient,
         borderRadius: radius,
         border: border,
